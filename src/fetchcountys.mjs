1231b2a3aa35ff60ded04310051c9c6e6b4c0579
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-17 20:52:11 +0100

    manuel sort

--- src/fetchcountys.mjs
+++ src/fetchcountys.mjs
@@ -6,7 +6,7 @@ const dir = "./build/countys/";
 const file = "index.json";
 
 const endpoint =
-  "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?outFields=last_update%2CRS%2Ccases7_per_100k%2CGEN%2CBEZ&returnGeometry=false&f=json&outSR=4326&where=1=1&orderByFields=RS%20asc";
+  "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?outFields=RS%2Clast_update%2CRS%2Ccases7_per_100k%2CGEN%2CBEZ&returnGeometry=false&f=json&outSR=4326&where=1=1&orderByFields=RS%20desc";
 
 const getNewCasesUrl = (RS) =>
   `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=(NeuerFall%20IN(1%2C%20-1))%20AND%20(IdLandkreis%3D%27${RS}%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlFall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true`;
@@ -53,6 +53,9 @@ fetch(endpoint)
         if (err) throw err;
       });
     }
+    finalJson.locations.sort((a, b) => {
+      return a.RS - b.RS;
+  });
     fs.writeFileSync(`${dir}${file}`, JSON.stringify(finalJson));
     console.log(
       '\x1b[42m\x1b[30m%s\x1b[0m',
