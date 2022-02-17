737d00320b59cc7f0d7d0cfa4b489004347cc8fc
ingomc <janick2002@gmx.de>
2022-02-17 11:30:04 +0100

    update fetch new

--- src/fetchstates.mjs
+++ src/fetchstates.mjs
@@ -7,13 +7,12 @@ const dir = "./build/states/";
 const file = "index.json";
 
 const endpoint =
-  "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=OBJECTID,LAN_ew_AGS,LAN_ew_GEN,LAN_ew_BEZ,LAN_ew_EWZ,Fallzahl,Aktualisierung,Death,cases7_bl_per_100k,cases7_bl,death7_bl&orderByFields=LAN_ew_GEN%20asc&cacheHint=true02100&resultOffset=0&resultRecordCount=25&resultType=standard&cacheHint=true";
-const getEndpointNewCases = (state) => {
-  return `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=(NeuerFall%20IN(1%2C%20-1))%20AND%20(Bundesland%3D%27${state}%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlFall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true`;
+  "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=OBJECTID,AdmUnitId,LAN_ew_AGS,LAN_ew_GEN,LAN_ew_BEZ,LAN_ew_EWZ,Fallzahl,Aktualisierung,Death,cases7_bl_per_100k,cases7_bl,death7_bl&orderByFields=LAN_ew_GEN%20asc&cacheHint=true02100&resultOffset=0&resultRecordCount=25&resultType=standard&cacheHint=true";
+
+const getEndpointNewCases = (AdmUnitId) => {
+  return `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_blbrdv/FeatureServer/0/query?f=json&where=(AnzAktivNeu%3C%3E0)%20AND%20(AdmUnitId%3D${AdmUnitId})&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=AdmUnitId%20asc&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true`;
 };
-const getEndpointNewDeaths = (state) => {
-  return `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=(NeuerTodesfall%20IN(1%2C%20-1))%20AND%20(Bundesland%3D%27${state}%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlTodesfall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true`;
-};
+
 
 // Todo: neue fälle heute, neue tode
 
@@ -25,33 +24,36 @@ const finalJson = {
 // called inside fetch, fetches new cases, pushes to json
 const handleData = async (stateData) => {
   // LAN_ew_GEN
-
   const newCases = await fetch(
-    getEndpointNewCases(encodeURI(stateData.LAN_ew_GEN))
+    getEndpointNewCases(encodeURI(stateData.AdmUnitId))
   )
     .then((res) => res.json())
-    .then((_json) => _json.features[0].attributes.value)
+    .then((_json) => _json.features[0].attributes.AnzFallNeu)
     .catch((error) => {
       console.log(
         "\x1b[31m%s\x1b[0m",
         ` x Error fetching handleData: fetch(getEndpointNewCases)`
       );
       console.log(error);
-      throw new Error(" x Error fetching handleData: fetch(getEndpointNewCases)");
+      throw new Error(
+        " x Error fetching handleData: fetch(getEndpointNewCases)"
+      );
     });
 
   const newDeaths = await fetch(
-    getEndpointNewDeaths(encodeURI(stateData.LAN_ew_GEN))
+    getEndpointNewCases(encodeURI(stateData.AdmUnitId))
   )
     .then((res) => res.json())
-    .then((_json) => _json.features[0].attributes.value)
+    .then((_json) => _json.features[0].attributes.AnzTodesfallNeu)
     .catch((error) => {
       console.log(
         "\x1b[31m%s\x1b[0m",
         ` x Error fetching handleData: fetch(getEndpointNewDeaths)`
       );
       console.log(error);
-      throw new Error("x Error fetching handleData: fetch(getEndpointNewDeaths)");
+      throw new Error(
+        "x Error fetching handleData: fetch(getEndpointNewDeaths)"
+      );
     });
 
   stateData.last_update = `${moment(stateData.Aktualisierung).format(
